e816f251508d5b3c5c4e7c3d1e70712d53dac624
MahendraDwivedi07 <dwivedimahendra9876@gmail.com>
2025-03-12 01:00:25 +0530

    second commit

---
+++ src/pages/educator/DashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DashBoard = () => {
+  return (
+    <div>
+        <h1>
+            Educator DashBoard
+        </h1>
+    </div>
+  )
+}
+
+export default DashBoard
---
+++ src/pages/student/CourseList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CourseList = () => {
+  return (
+    <div>
+        <h1>
+        CourseList
+        </h1>
+    </div>
+  )
+}
+
+export default CourseList
